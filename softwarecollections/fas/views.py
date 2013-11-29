d5d67cfff4d3e6ab6f06ecf5e19958cafcb27e71
Jakub Dorňák <jdornak@redhat.com>
2013-11-29 18:59:25 +0100

    fas without name conflicts and decorator magic

--- softwarecollections/fas/views.py
+++ softwarecollections/fas/views.py
@@ -1,63 +1,46 @@
-from django.contrib.auth import login, REDIRECT_FIELD_NAME
+from django.conf import settings
+from django.contrib.auth import login as auth_login, REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.views.decorators.http import require_POST
 
+from social.utils import module_member
+from social.backends.utils import get_backend
 from social.actions import do_auth, do_complete, do_disconnect
 
 from django.core.urlresolvers import reverse
-from social.apps.django_app.utils import load_strategy
-from functools import wraps
+from social.apps.django_app.utils import BACKENDS, STORAGE, STRATEGY
 
-def strategy(redirect_uri=None, load_strategy=load_strategy):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(request, *args, **kwargs):
-            uri = redirect_uri
-            if uri and not uri.startswith('/'):
-                uri = reverse(redirect_uri)
-
-            request.social_strategy = load_strategy(
-                request=request, backend='openid',
-                redirect_uri=uri, *args, **kwargs
-            )
-            request.social_strategy.backend.URL = 'id.fedoraproject.org'
-
-            # backward compatibility in attribute name, only if not already
-            # defined
-            if not hasattr(request, 'strategy'):
-                request.strategy = request.social_strategy
-            return func(request, *args, **kwargs)
-        return wrapper
-    return decorator
+Backend  = get_backend(BACKENDS, 'openid')
+Backend.URL = 'id.fedoraproject.org'
+Storage  = module_member(STORAGE)
+Strategy = module_member(STRATEGY)
 
 
-@strategy('fas:complete')
-def login(request):
-    return do_auth(request.social_strategy, redirect_name=REDIRECT_FIELD_NAME)
+def login(request, *args, **kwargs):
+    strategy = Strategy(Backend, Storage, request, backends=BACKENDS,
+                    redirect_uri=reverse('fas:complete'), *args, **kwargs)
+    return do_auth(strategy, redirect_name=REDIRECT_FIELD_NAME)
 
 
 @csrf_exempt
-@strategy('fas:complete')
 def complete(request, *args, **kwargs):
-    """Authentication complete view, override this view if transaction
-    management doesn't suit your needs."""
-    return do_complete(request.social_strategy, _do_login, request.user,
+    strategy = Strategy(Backend, Storage, request, backends=BACKENDS,
+                    redirect_uri=reverse('fas:complete'), *args, **kwargs)
+    return do_complete(strategy, _do_login, request.user,
                        redirect_name=REDIRECT_FIELD_NAME, *args, **kwargs)
 
 
-#@require_POST
-#@csrf_protect
 @login_required
-@strategy()
 def logout(request, association_id=None):
-    """Disconnects fas from current logged in user."""
-    return do_disconnect(request.social_strategy, request.user, association_id,
+    strategy = Strategy(Backend, Storage, request, backends=BACKENDS,
+                    redirect_uri=None)
+    return do_disconnect(strategy, request.user, association_id,
                          redirect_name=REDIRECT_FIELD_NAME)
 
 
 def _do_login(strategy, user):
-    login(strategy.request, user)
+    auth_login(strategy.request, user)
     # user.social_user is the used UserSocialAuth instance defined in
     # authenticate process
     social_user = user.social_user
