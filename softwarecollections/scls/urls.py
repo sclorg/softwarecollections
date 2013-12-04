12c0b16dddb0fd0da1b37fdfad3d740b235eafdf
Jakub Dorňák <jdornak@redhat.com>
2013-12-04 17:07:58 +0100

    more on views

--- softwarecollections/scls/urls.py
+++ softwarecollections/scls/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('softwarecollections.scls.views',
-    url(r'^$',      'roster',   name='all', kwargs={'type':'all'}),
-    url(r'^my/$',   'roster',   name='my',  kwargs={'type':'my'}),
-    url(r'^new/$',  'new',      name='new'),
-    url(r'^detail/(?P<slug>.*)/$',
-                    'detail',   name='detail'),
+    url(r'^$',                          'all',      name='all'),
+    url(r'^user/$',                     'my',       name='my'),
+    url(r'^user/(?P<username>.*)/$',    'user',     name='user'),
+    url(r'^new/$',                      'new',      name='new'),
+    url(r'^detail/(?P<slug>.*)/$',      'detail',   name='detail'),
 )
--- softwarecollections/scls/views.py
+++ softwarecollections/scls/views.py
@@ -1,21 +1,39 @@
 from django.conf import settings
 from django.http.response import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template.base import TemplateDoesNotExist
 
 from django.views.generic import CreateView, DetailView, ListView
 from .models import SoftwareCollection
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class Roster(ListView):
+    view = None
     model = SoftwareCollection
     paginate_by = 10
 
-roster = Roster.as_view()
+    def get_queryset(self):
+        if   self.view == 'all':
+            return self.model._default_manager.all()
+        elif self.view == 'my':
+            return self.model._default_manager.filter(maintainer = self.request.user)
+        elif self.view == 'user':
+            user = get_object_or_404(User, **{User.USERNAME_FIELD: self.kwargs['username']}) 
+            return self.model._default_manager.filter(maintainer = user)
+
+all  = Roster.as_view(view='all')
+my   = login_required(Roster.as_view(view='my'))
+user = Roster.as_view(view='user')
 
 
 class Detail(DetailView):
     model = SoftwareCollection
+    context_object_name = 'scl'
 
 detail = Detail.as_view()
 
@@ -23,5 +41,5 @@ detail = Detail.as_view()
 class New(CreateView):
     model = SoftwareCollection
 
-new = New.as_view()
+new = login_required(New.as_view())
 
