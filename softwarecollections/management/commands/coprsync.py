1d223a1ec057011eb7a56c1fd13e10c166a56f5e
Matej Stuchlik <mstuchli@redhat.com>
2013-12-04 09:42:33 +0100

    Add cli subcommand for syncing COPR repos
    Added utils.py with with parts of msrb's coprsync script which
    could potentially be reused and implemented the rest in coprsync.py
    in commands.

---
+++ softwarecollections/management/commands/coprsync.py
@@ -0,0 +1,27 @@
+from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+from softwarecollections.utils import get_copr, generate_repo_config, run_reposync
+
+DEFAULT_COPR_API_URL = "http://copr-fe-dev.cloud.fedoraproject.org/api"
+
+class Command(BaseCommand):
+    args = '<username> <coprname> <destdir>'
+    help = 'Synces COPR repo to specified directory.'
+
+    option_list = BaseCommand.option_list + (
+            make_option('--apiurl', help='Override COPR API URL', default=DEFAULT_COPR_API_URL),
+            )
+
+    def handle(self, *args, **options):
+        if len(args) != 3:
+            raise CommandError("need exactly 3 arguments")
+
+        username, coprname, destdir = args
+        self.stdout.write("Syncing {}-{} to {}".format(*args))
+
+        copr = get_copr(username, coprname, options.get('apiurl'))
+        configs = generate_repo_config(copr, username)
+        run_reposync(configs, destdir)
+
+        self.stdout.write("Done")
