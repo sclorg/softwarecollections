44731021f8f6149ae44fd1f58625e0b324351657
Jakub Dorňák <jdornak@redhat.com>
2013-12-10 21:06:58 +0100

    require login for view scls:list_my

--- softwarecollections/scls/views.py
+++ softwarecollections/scls/views.py
@@ -12,7 +12,6 @@ from tagging.models import Tag
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
 
 
 def _list(request, template, queryset, dictionary, **kwargs):
@@ -30,16 +29,19 @@ def list_all(request, **kwargs):
     return _list(request, 'scls/list_all.html', queryset, {}, **kwargs)
 
 
+@login_required
 def list_my(request, **kwargs):
     queryset = SoftwareCollection.objects.filter(maintainer = request.user)
     return _list(request, 'scls/list_my.html', queryset, {}, **kwargs)
 
 
 def list_user(request, username, **kwargs):
+    User = get_user_model()
     user = get_object_or_404(User, **{User.USERNAME_FIELD: username})
     queryset = SoftwareCollection.objects.filter(maintainer = user)
     dictionary = {'user': user}
     return _list(request, 'scls/list_user.html', queryset, dictionary, **kwargs)
+
 
 def list_tag(request, name, **kwargs):
     try:
