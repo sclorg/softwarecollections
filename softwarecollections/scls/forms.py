1704af218482715afd576635ae65eda81bfcee4d
Miroslav Suchý <msuchy@redhat.com>
2013-12-16 13:54:13 +0100

    add real policy texts

--- softwarecollections/scls/forms.py
+++ softwarecollections/scls/forms.py
@@ -4,15 +4,19 @@ from django.utils.translation import ugettext_lazy as _
 from .models import SoftwareCollection
 
 POLICY_CHOICES = tuple((s, s) for s in [
-    '''quick and dirty
-Lorem ipsum dolor sit amet, consectetur
-adipisicing elit, sed do eiusmod tempor incididunt ut labore
-et dolore magna aliqua. Ut enim ad minim veniam.''',
-    '''stable
-Lorem ipsum dolor sit amet, consectetur
-adipisicing elit, sed do eiusmod tempor incididunt ut labore
-et dolore magna aliqua. Ut enim ad minim veniam.
-''',
+    '''**Developing** - this is just for my testing purposes and it probably does not work yet.
+And if this works I will put here update soon, which will most likely break it.
+    ''',
+    '''**Quick and Dirty** - I made this collection just for myself and few
+of my colleagues. Do not expect bug-fixes, security patches or any kind of support.
+    ''',
+    '''**Just Works** - I plan to use this collection very often. You may
+expect bug-fixes here. But only if my time allows me.
+    ''',
+    '''**Professional** - I will maintain this collections regularly. All fixes
+will be done in timely manner. And security fixes will be back-ported as soon
+as possible.
+    ''',
 ])
 
 class CreateForm(ModelForm):
