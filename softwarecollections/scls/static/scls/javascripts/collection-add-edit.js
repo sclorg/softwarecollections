d9d247ad323930a612d8583c376c4627da119b25
Slavek Kabrda <bkabrda@redhat.com>
2014-03-07 15:31:49 +0100

    Don't refresh copr projects list if it's already loaded

--- softwarecollections/scls/static/scls/javascripts/collection-add-edit.js
+++ softwarecollections/scls/static/scls/javascripts/collection-add-edit.js
@@ -2,6 +2,10 @@ $(function() {
     // we'll use a timer to load repo list whenever user stops typing
     // for 0.6 seconds (this number just feels right ;))
     var timer;
+    // remember last synced copr username, so that we don't refresh
+    // unnecessarily, e.g. after a blur that follows keyup
+    var last_synced_username;
+    var last_sync_successful;
 
     $("#id_copr_username").on("keyup blur", function() {
         // coprnames_url has to be defined in template, since it's processed
@@ -10,8 +14,13 @@ $(function() {
             clearTimeout(timer);
         }
         timer = setTimeout(function() {
-            var url = coprnames_url.replace("__copr_username__",
-                                            $("#id_copr_username").val());
+            var curr_name = $("#id_copr_username").val()
+            if (last_sync_successful && last_synced_username == curr_name) {
+                return
+            } else {
+                last_sync_successful = false
+            }
+            var url = coprnames_url.replace("__copr_username__", curr_name);
             var $select = $("#id_copr_name");
             $select.empty();
             $.get(url, function(projects) {
@@ -21,6 +30,9 @@ $(function() {
                             .text(projects[i])
                     );
                 }
+                // only set last_sync* if we were successful
+                last_synced_username = curr_name
+                last_sync_successful = true
             }, "json");
         }, 600);
     });
