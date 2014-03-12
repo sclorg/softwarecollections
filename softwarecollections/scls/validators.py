3db2d2e4ab0a2b7dccf4084949e81706cff53dcb
Jakub Dorňák <jdornak@redhat.com>
2014-03-12 11:20:17 +0100

    allow + in the name

--- softwarecollections/scls/validators.py
+++ softwarecollections/scls/validators.py
@@ -2,7 +2,6 @@ import re
 from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 
-name_re = re.compile(r'^[a-zA-Z0-9]([-a-zA-Z0-9_.]*[a-zA-Z0-9])?$')
-name_re = re.compile(r'^[a-zA-Z0-9]+$')
-validate_name = RegexValidator(name_re, _("Enter a valid name consisting of letters, numbers, underscores, hyphens or dots."), 'invalid')
+name_re = re.compile(r'^[a-zA-Z0-9][a-zA-Z0-9_.+-]*$')
+validate_name = RegexValidator(name_re, _('Enter a valid name consisting of letters, numbers, underscores, hyphens, pluses or dots.'), 'invalid')
 
