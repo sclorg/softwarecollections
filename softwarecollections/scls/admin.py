4df55979400ef3af872a84533ef4881571e8bbed
Jakub Dorňák <jdornak@redhat.com>
2014-08-04 16:51:02 +0200

    admin action request_sync

--- softwarecollections/scls/admin.py
+++ softwarecollections/scls/admin.py
@@ -6,7 +6,7 @@ class SoftwareCollectionAdmin(admin.ModelAdmin):
     list_display = ('slug', 'get_title_tag', 'get_copr_tag', 'review_req', 'approved', 'auto_sync', 'need_sync', 'last_synced', 'last_modified')
     list_filter  = ('review_req', 'approved', 'maintainer')
     ordering     = ('slug',)
-    actions      = ('approve',)
+    actions      = ('approve', 'request_sync')
 
     def approve(self, request, queryset):
         rows_updated = queryset.update(review_req = False, approved = True)
@@ -16,6 +16,15 @@ class SoftwareCollectionAdmin(admin.ModelAdmin):
             rows_updated) % {'count': rows_updated}
         )
     approve.short_description = _('Approve selected software collections')
+        
+    def request_sync(self, request, queryset):
+        rows_updated = queryset.update(need_sync = True)
+        self.message_user(request, ungettext(
+            'Sync was requested for %(count)d collection',
+            'Sync was requested for %(count)d collections',
+            rows_updated) % {'count': rows_updated}
+        )
+    request_sync.short_description = _('Request sync for selected software collections')
         
 admin.site.register(SoftwareCollection, SoftwareCollectionAdmin)
 admin.site.register(Repo)
