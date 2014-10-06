6189d82ea0d4ec95fcff0e466fade970b2603329
Adam Samalik <asamalik@redhat.com>
2014-10-06 15:16:13 +0200

    docs page divided + menu item macro
    It will help users to get informed about:
        - what scl is and how it works - page about
        - how to use and develop scls - page docs&guides
    
    Menu item to help tech writers with adding their content easily:
        {% menu_item "en/about/licensing" "Licensing Guidelines" %}

---
+++ softwarecollections/scls/templatetags/menu_item.py
@@ -0,0 +1,14 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.simple_tag(takes_context = True)
+def menu_item(context, path, name):
+    request = context["request"]
+    active = "active" if request.path == "/{path}/".format(path=path) else ""
+    item = "<li class='{active}' role='menuitem'>" \
+           "    <a href='/{path}/'>{name}</a>" \
+           "</li>".format(name=name, path=path, active=active)
+
+    return mark_safe(item)
