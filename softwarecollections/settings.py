2e642886710e62ac335a30ae4fefb904b4180055
Jakub Dorňák <jdornak@redhat.com>
2015-06-08 17:19:28 +0200

    use KEY_PREFIX for Memcache

--- softwarecollections/settings.py
+++ softwarecollections/settings.py
@@ -136,6 +136,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
+        'KEY_PREFIX': 'softwarecollections',
     }
 }
 
