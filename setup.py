6c9b7ecacca273caec51f06488fbcd8f27b20a46
Jan Staněk <jstanek@redhat.com>
2018-09-05 13:52:25 +0200

    Make the package pip installable
    Fill setup.py to match the dependencies specified in spec file,
    which will allow the usage of virtual environments for local
    development.
    
    Related: sclorg#107

--- setup.py
+++ setup.py
@@ -1,17 +1,54 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-import softwarecollections
-#from distutils.core import setup
+import os
 from setuptools import setup, find_packages
 
+REPO_DIR = os.path.dirname(os.path.abspath(__file__))
+
+METADATA = {
+    "name": "softwarecollections",
+    "version": "0.16",
+    "author": "Jakub Dorňák",
+    "author_email": "jakub.dornak@misli.cz",
+    "maintainer": "Jan Staněk",
+    "maintainer_email": "jstanek@redhat.com",
+    "url": "https://github.com/sclorg/softwarecollections",
+}
+
+# Basic/common package dependencies
+REQUIRES = [
+    "django-fas",
+    "django-markdown2",
+    "django-sekizai",
+    "django-simple-captcha",
+    "django-tagging",
+    "django<1.11",
+    "flock",
+    "py3dns",  # pylibravatar missing dependency workaround
+    "pylibravatar",
+    "python3-memcached",
+    "python3-openid",
+    "requests",
+]
+
+# Extra dependencies for production
+PROD_REQUIRES = [
+    "mod_wsgi",
+    "psycopg2",
+]
+
+
+with open(os.path.join(REPO_DIR, "README.md"), encoding="utf-8") as readme:
+    long_description = readme.read()
+
 setup(
-    name         = "softwarecollections",
-    version      = '0.16',
-    description  = "Software Collection Management Website and Utils",
-    author       = "Jakub Dorňák",
-    author_email = "jdornak@redhat.com",
-    url          = "https://github.com/sclorg/softwarecollections",
-    packages     = find_packages(),
-    include_package_data = True,
+    **METADATA,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3",
+    install_requires=REQUIRES,
+    extras_require={"production": PROD_REQUIRES},
 )
