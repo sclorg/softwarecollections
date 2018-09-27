90acf2d030be77ca98d0520785664d8409520ff8
Jan Staněk <jstanek@redhat.com>
2018-09-27 16:11:38 +0200

    Load settings from environment
    - Unify settings to single module/package
    - Introduce utility module for processing environment variables
    - Load settings from environment variables, with development defaults

---
+++ softwarecollections/settings/env_util.py
@@ -0,0 +1,114 @@
+"""Settings utilities for working with environment variables"""
+
+import re
+import os
+from distutils.util import strtobool
+from urllib.parse import urlparse, uses_netloc
+from pathlib import Path
+from typing import Optional, Pattern, Sequence, Tuple
+
+import dj_database_url
+from django.core.management.utils import get_random_secret_key
+
+# Extra types
+EmailSequence = Sequence[Tuple[str, str]]
+
+# E-mail parsing
+EMAIL_SEPARATOR = re.compile(r",\s?")
+EMAIL_PARTS = re.compile(r"(?P<fullname>[\w\s]+)<(?P<email>[^@]+@[^@]+\.[^@]+)>")
+
+# Cache URL support
+CACHE_BACKEND = {
+    "memcached": "django.core.cache.backends.memcached.MemcachedCache",
+    "locmem": "django.core.cache.backends.locmem.LocMemCache",
+}
+
+uses_netloc.extend(CACHE_BACKEND.keys())
+
+
+def load_boolean(envvar: str, default: bool = False) -> bool:
+    """Load boolean environment variable"""
+
+    value = os.getenv(envvar, str(default).lower())
+    return strtobool(value)
+
+
+def load_path(envvar: str, default: Path) -> Path:
+    """Load file system path from environment"""
+
+    path = Path(os.getenv(envvar, str(default)))
+    return path.resolve()
+
+
+def load_sequence(
+    envvar: str, separator: Pattern = re.compile(r":"), default: Sequence[str] = ()
+) -> Sequence[str]:
+    """Load sequence of strings from environment (i.e. PATH)"""
+
+    value = os.getenv(envvar)
+    if value is None:
+        return default
+    else:
+        return separator.split(value)
+
+
+def load_email_sequence(
+    envvar: str, default: Sequence[Tuple[str, str]] = []
+) -> Sequence[Tuple[str, str]]:
+    """Load sequence of e-mail addresses from environment.
+
+    Expected format: User One <a@ex.com>, User Two <b@e.mail>
+    """
+
+    value_list = load_sequence(envvar, separator=EMAIL_SEPARATOR)
+    match_iter = filter(None, map(EMAIL_PARTS.search, value_list))
+
+    return [(m["fullname"].strip(), m["email"].strip()) for m in match_iter]
+
+
+def load_secret_key(
+    envvar: str, keyfile: Optional[Path] = None, default: Optional[str] = None
+) -> str:
+    """Load SECRET_KEY from environment or file.
+
+    Priority:
+        1. Environment variable.
+        2. Contents of key file.
+        3. Default value (randomly generated if not provided).
+    """
+
+    key = os.getenv(envvar, None)
+    if key is not None:
+        return key
+
+    if keyfile is not None and keyfile.is_file():
+        return keyfile.read_text(encoding="utf-8").strip()
+
+    if default is not None:
+        return default
+    else:
+        return get_random_secret_key()
+
+
+def load_database_url(envvar: str, default: str = "sqlite://:memory:") -> dict:
+    """Load and parse database URL.
+
+    Returns: Complete configuration dictionary.
+    """
+
+    # just wrap to have a consistent interface
+    return dj_database_url.config(env=envvar, default=default)
+
+
+def load_cache_url(envvar: str, default: str = "locmem://") -> dict:
+    """Load and parse cache URL.
+
+    Returns: Complete configuration dictionary.
+    """
+
+    parsed = urlparse(os.getenv(envvar, default=default))
+    try:
+        return {"BACKEND": CACHE_BACKEND[parsed.scheme], "LOCATION": parsed.netloc}
+    except KeyError as err:
+        message = "Unknown cache type: '{!s}'".format(err)
+        raise ValueError(message) from err
---
+++ tests/test_settings.py
@@ -0,0 +1,174 @@
+"""Test loading information from environment vars"""
+
+import importlib
+from pathlib import Path
+
+import pytest
+
+
+def env_fixture(envvar: str, *values):
+    """Generate fixture for environment variable variants."""
+
+    @pytest.fixture(name=envvar, params=values)
+    def _env_fixture(request, monkeypatch):
+        if request.param is None:
+            monkeypatch.delenv(envvar, raising=False)
+        else:
+            monkeypatch.setenv(envvar, request.param)
+        return request.param
+
+    return _env_fixture
+
+
+@pytest.fixture
+def scl_settings():
+    """Force load SCL settings from disk"""
+
+    spec = importlib.util.find_spec("softwarecollections.settings")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    return module
+
+
+SCL_BASE_DIR = env_fixture("SCL_BASE_DIR", None, "/var/scls")
+SCL_SECRET_KEY = env_fixture("SCL_SECRET_KEY", None, "abcdef" * 4)
+SCL_DEBUG = env_fixture("SCL_DEBUG", None, "true", "false")
+SCL_DBDEBUG = env_fixture("SCL_DBDEBUG", None, "true", "false")
+SCL_ALLOWED_HOSTS = env_fixture("SCL_ALLOWED_HOSTS", None, "single", "one:two:three")
+SCL_ADMINS = env_fixture(
+    "SCL_ADMINS",
+    None,
+    "One <one@example.com>",
+    "One <one@example.com>, Two <two@example.com>",
+)
+SCL_DATABASE_URL = env_fixture(
+    "SCL_DATABASE_URL", None, "postgres://%2fvar%2fscls:5432/scldb"
+)
+SCL_CACHE_URL = env_fixture("SCL_CACHE_URL", None, "memcached://localhost:11211")
+
+
+@pytest.fixture(params=[None, "aa" * 8])
+def SCL_SECRET_KEY_FILE(request, monkeypatch, tmpdir):
+    if request.param is None:
+        monkeypatch.delenv("SCL_SECRET_KEY_FILE", raising=False)
+        return None
+    else:
+        path = tmpdir.mkdir("scls").join("secret_key")
+        path.write(request.param.encode("utf-8"))
+        monkeypatch.setenv("SCL_SECRET_KEY_FILE", str(path))
+        return str(path)
+
+
+def test_base_dir(SCL_BASE_DIR, scl_settings):
+    """The BASE_DIR is set to expected value"""
+
+    if SCL_BASE_DIR is None:
+        expected = Path(__file__).parents[1]
+    else:
+        expected = Path(SCL_BASE_DIR)
+
+    assert scl_settings.BASE_DIR == expected
+
+
+def test_secret_key_is_loaded(SCL_SECRET_KEY, SCL_SECRET_KEY_FILE, scl_settings):
+    """The secret key is loaded from external source"""
+
+    if SCL_SECRET_KEY is not None:
+        assert scl_settings.SECRET_KEY == SCL_SECRET_KEY
+        return  # Ignore other relevant environment variables
+
+    if SCL_SECRET_KEY_FILE is not None:
+        assert (
+            scl_settings.SECRET_KEY
+            == Path(SCL_SECRET_KEY_FILE).read_text(encoding="utf-8").strip()
+        )
+        return  # Ignore default
+
+    # if both envvars are not set, assert that it still has non-empty value
+    assert scl_settings.SECRET_KEY
+
+
+def test_debug(SCL_DEBUG, scl_settings):
+    """Expected debug parameter is set"""
+
+    if SCL_DEBUG == "true" or SCL_DEBUG is None:
+        assert scl_settings.DEBUG
+    else:
+        assert not scl_settings.DEBUG
+
+
+def test_dbdebug(SCL_DBDEBUG, scl_settings):
+    """Expected database debug parameter is set"""
+
+    if SCL_DBDEBUG == "true":
+        assert scl_settings.DBDEBUG
+    else:
+        assert not scl_settings.DBDEBUG
+
+
+def test_allowed_hosts(SCL_ALLOWED_HOSTS, scl_settings):
+    """Allowed hosts are set as expected"""
+
+    if SCL_ALLOWED_HOSTS is None:
+        assert scl_settings.ALLOWED_HOSTS == ["*"]
+    else:
+        assert scl_settings.ALLOWED_HOSTS == SCL_ALLOWED_HOSTS.split(":")
+
+
+def test_admin_emails(SCL_ADMINS, scl_settings):
+    """Administrator addresses are set as expected"""
+
+    assert isinstance(scl_settings.ADMINS, list)
+
+    if SCL_ADMINS is None:
+        assert len(scl_settings.ADMINS) == 0
+    else:
+        assert len(scl_settings.ADMINS) >= 1
+        assert scl_settings.ADMINS[0] == ("One", "one@example.com")
+
+
+def test_database(SCL_DATABASE_URL, scl_settings):
+    """Database configuration is loaded from an URL"""
+
+    if SCL_DATABASE_URL is None:
+        expected = {
+            "ENGINE": "django.db.backends.sqlite3",
+            "HOST": "",
+            "USER": "",
+            "PASSWORD": "",
+            "NAME": str(scl_settings.BASE_DIR / "data" / "db.sqlite3"),
+            "PORT": "",
+            "CONN_MAX_AGE": 0,
+        }
+    elif SCL_DATABASE_URL.startswith("postgres"):
+        expected = {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "HOST": "/var/scls",
+            "USER": "",
+            "PASSWORD": "",
+            "NAME": "scldb",
+            "PORT": 5432,
+            "CONN_MAX_AGE": 0,
+        }
+
+    assert scl_settings.DATABASES["default"] == expected
+
+
+def test_cache(SCL_CACHE_URL, scl_settings):
+    """Cache URL is parsed properly"""
+
+    if SCL_CACHE_URL is None:
+        expected = {
+            "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+            "LOCATION": "scl-devel",
+            "KEY_PREFIX": "softwarecollections",
+        }
+    elif SCL_CACHE_URL.startswith("memcached"):
+        expected = {
+            "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+            "LOCATION": "localhost:11211",
+            "KEY_PREFIX": "softwarecollections",
+        }
+
+    assert scl_settings.CACHES["default"] == expected
