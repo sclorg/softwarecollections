7e6caf5ed3d3dcc10ed187690cd3d8f88a6ca008
Jan Staněk <jstanek@redhat.com>
2019-03-29 15:53:53 +0100

    Work around issues with test database migrations on Django 1.*

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,26 @@
 """py.test global configuration"""
 
 import pytest
+from pkg_resources import get_distribution, parse_version
 
 from django.core.management import call_command
 from django.db import connection
+
+DJANGO_VERSION = get_distribution("Django").parsed_version
+
+
+@pytest.fixture(scope="session")
+def django_db_use_migrations(django_db_use_migrations):
+    """Older Django (<2.0) has trouble with migrations on newer SQLite.
+
+    The current workaround is to disable migrations on Django 1.*.
+    Possibly related Django issue: https://code.djangoproject.com/ticket/29182
+    """
+
+    if DJANGO_VERSION < parse_version("2.0"):
+        return False
+    else:
+        return django_db_use_migrations
 
 
 @pytest.fixture(scope="session")
